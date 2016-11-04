33b8f1cf31cee85188e3c344292c8689bf21c196
Daniel Strunk <daniel@strunk.me>
2016-11-04 16:33:27 -0400

    Add Google Search Engine to site
    Add Google's custom search engine to the site, and style results to resemble the
    rest of the site. A couple of links helped with the current functionality of
    having the search results show up via a popup on the same page:
    
    http://stackoverflow.com/questions/10021761/loading-google-custom-search-results-without-page-refresh
    
    Closes #7.

---
+++ js/search.js
@@ -0,0 +1,33 @@
+
+$(function() {
+  window.__gcse = {
+    callback: googleCSELoaded
+  };
+
+  function googleCSELoaded() {
+
+    $("#search-button").on('click', function() {
+      var searchText = $("#search-text").val();
+      var element    = google.search.cse.element.getElement("searchOnlyCSE");
+      element.execute(searchText);
+    });
+
+    $("#search-text").on('keyup', function(e) {
+      if (e.keyCode == 13) {
+        var searchText = $("#search-text").val();
+        var element    = google.search.cse.element.getElement("searchOnlyCSE");
+        element.execute(searchText);
+      }
+    })
+  }
+
+  (function() {
+    var cx = '005588353414550359789:csnxl5bz9jw';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = '//cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+});
