52bcff1f9e90238f55aad32ee3b39bda730338bc
Daniel Strunk <daniel@strunk.me>
2018-06-19 23:33:23 -0400

    Nuke from orbit
    Remove all posts, layout settings, etc, and start over fresh. Instead of
    *completely* removing previous blog posts, Add them to a folder
    that is not currently output: `_archive`. Other than that, everything is
    starting from scratch.
    
    Add initial Tailwind and Laravel Mix configuration, alongside Vue, Axios
    and friends.

--- js/app.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $mobileIcon = $('a[data-application-nav-mobile-icon]');
-  $header      = $('header[data-application-header]');
-
-  $mobileIcon.on('click', function(e) {
-    e.preventDefault();
-    $header.toggleClass('active');
-  });
-});
---
+++ webpack.mix.js
@@ -0,0 +1,61 @@
+let mix = require('laravel-mix');
+let tailwindcss = require('tailwindcss');
+let glob = require('glob-all');
+let fs = require('fs-extra');
+let PurgecssPlugin = require('purgecss-webpack-plugin');
+
+// Custom PurgeCSS extractor for Tailwind that allows special characters in
+// class names.
+//
+// https://github.com/FullHuman/purgecss#extractor
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:\/]+/g) || [];
+  }
+}
+
+mix.setPublicPath('assets')
+    .js('_resources/js/app.js', 'js/')
+    .less('_resources/less/app.less', 'css/')
+    .options({
+        postCss: [
+            tailwindcss('./tailwind.js'),
+        ]
+    })
+    .browserSync({
+        browser: 'Google Chrome',
+        proxy: false,
+        server: {
+            baseDir: '_site/',
+        },
+        files: ['_site/**/*.html', '_site/**/*.js', '_site/**/*.css']
+    })
+    .disableNotifications()
+    .then(function () {
+        fs.move('assets/mix-manifest.json', '_data/mix-manifest.json', { overwrite: true })
+        .then(() => console.log('mix-manifest.json copied to _data/'))
+    })
+
+if (mix.inProduction()) {
+    mix.webpackConfig({
+        plugins: [
+            new PurgecssPlugin({
+
+                // Specify the locations of any files you want to scan for class names.
+                paths: glob.sync([
+                    path.join(__dirname, '_site/**/*.html'),
+                    path.join(__dirname, '_site/**/*.js')
+                ]),
+                extractors: [
+                    {
+                        extractor: TailwindExtractor,
+
+                        // Specify the file extensions to include when scanning for
+                        // class names.
+                        extensions: ['html', 'js', 'vue']
+                    }
+                ]
+            })
+        ]
+    });
+}
