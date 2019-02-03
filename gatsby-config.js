57dd375a5397f6c3dc23d61698943af114129fc7
Daniel Strunk <daniel@strunk.me>
2019-02-02 20:05:54 -0500

    Update site config for blog functionality
    Remove some of the defaults that come with a new Gatsby install. Add
    some things that will be needed for good internet citizenship: sitemaps,
    actual CSS, etc.
    
    One of the largest changes in this commit is the `gatsby-node.js` file:
    it has been configured to create blog posts for every `*.md` file in the
    content directory. Each directory is treated as a separate "content
    type" instance, so I can have `/blog`, `/journal`, and `/til` as
    separate blog entry points.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,41 +1,111 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    siteUrl: 'https://danielstrunk.me',
+    title: `Daniel Strunk`,
+    description: 'The personal site of Daniel Strunk, a full-stack web developer currently working on Magento 2 and Laravel.',
+    author: '@dstrunk',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'journal',
+        path: `${__dirname}/content/journal`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'til',
+        path: `${__dirname}/content/til`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'blog',
+        path: `${__dirname}/content/blog`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-transformer-remark',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: 'gatsby-remark-responsive-iframe',
+            options: {
+              wrapperStyle: 'margin-bottom: 1.0725rem',
+            },
+          },
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+        ],
       },
     },
-    `gatsby-plugin-less`,
-    `gatsby-plugin-postcss`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-purgecss`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
+        name: 'daniel-strunk',
+        short_name: 'dstrunk',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+      },
+    },
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        typeName: "GitHub",
+        fieldName: "github",
+        // Url to query from
+        url: "https://api.github.com/graphql",
+        // HTTP headers
+        headers: {
+          // Learn about environment variables: https://gatsby.app/env-vars
+          Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
+        },
+        // Additional options to pass to node-fetch
+        fetchOptions: {},
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-less',
+      options: {
+        postCssPlugins: [require('tailwindcss')('./tailwind.js')],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-purgecss',
+      options: {
+        develop: true,
         tailwind: true,
+        printRejected: true,
         purgeOnly: ['src/css/style.css'],
       },
     },
+    'gatsby-plugin-sitemap',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,128 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`)
+const slugify = require('limax')
 
-// You can delete this file if you're not using it
+const getType = node => {
+  const { fileAbsolutePath } = node
+  const [, type] = fileAbsolutePath
+    .split(path.resolve('content'))
+    .pop()
+    .split('/')
+
+  return type
+}
+
+const getDate = node => {
+  const { fileAbsolutePath } = node
+  const [, _type, date] = fileAbsolutePath
+    .split(path.resolve('content'))
+    .pop()
+    .split('/')
+
+  return date
+}
+
+const getJekyllDate = node => {
+  const { fileAbsolutePath } = node
+
+  try {
+    const [, date] = fileAbsolutePath
+      .split('/')
+      .pop()
+      .match(
+        /([\d]{4}-[\d]{2}-[\d]{2})/
+      )
+
+    return date
+  } catch (err) {
+    // Swallow the error, this should only happen for old Jekyll posts
+  }
+}
+
+exports.onCreateNode = ({
+  actions: { createNodeField },
+  node,
+})=> {
+  if (node.internal.type === 'MarkdownRemark') {
+    const slug = node.frontmatter.slug || slugify(node.frontmatter.title)
+
+    createNodeField({
+      node,
+      name: 'slug',
+      value: slug,
+    })
+
+    createNodeField({
+      node,
+      name: 'blogpath',
+      value: `${getType(node)}/${getDate(node)}/${slug}`,
+    })
+
+    createNodeField({
+      node,
+      name: 'type',
+      value: getType(node),
+    })
+
+    createNodeField({
+      node,
+      name: 'jekyllDate',
+      value: getJekyllDate(node),
+    })
+  }
+}
+
+exports.createPages = ({ actions, graphql, getNode }) => {
+  const { createPage } = actions
+
+  const postTemplate = path.resolve('src/templates/post.js')
+
+  return graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: frontmatter___date }
+      ) {
+        edges {
+          node {
+            excerpt(pruneLength: 160)
+            html
+            id
+            fields {
+              slug
+              type
+              blogpath
+              jekyllDate
+            }
+            frontmatter {
+              title
+              slug
+              date
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors)
+    }
+
+    const posts = result.data.allMarkdownRemark.edges
+
+    posts.forEach(({ node }) => {
+      const { fields: { slug, blogpath, type, jekyllDate } } = node
+
+      createPage({
+        path: blogpath,
+        component: postTemplate,
+        context: {
+          slug,
+          blogpath,
+          type,
+          jekyllDate,
+        },
+      })
+    })
+
+    return posts
+  })
+}
